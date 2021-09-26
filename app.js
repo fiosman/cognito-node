5e1fe6a76617fbd6c2d8042d7a05f94dfb65794e
fiosman92 <fares.osman@outlook.com>
2021-09-25 23:00:19 -0400

    Set up cognito sign up

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+const cognitoConfig = require("./config.json");
+
+const poolData = {
+  UserPoolId: cognitoConfig.cognito.userpoolId,
+  ClientId: cognitoConfig.cognito.clientId,
+};
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+
+//sign up
+app.post("/signup", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const dataEmail = {
+    Name: "email",
+    Value: email,
+  };
+
+  const attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+
+  userPool.signUp(email, password, [attributeEmail], null, (err, result) => {
+    if (err) {
+      res.json(err.message);
+      return;
+    }
+    const cognitoUser = result;
+    const sub = cognitoUser.userSub;
+    res.json(sub);
+  });
+});
+
+app.listen("3000", () => console.log("Now listening on port 30000"));
