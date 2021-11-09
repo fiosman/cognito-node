f456c5722e0eadf32fc28a5ab4b0b1f0a9d19fea
fiosman <fares.osman@outlook.com>
2021-11-08 23:47:13 -0500

    Update stripe

--- frontend/frontend/src/CheckoutForm.js
+++ frontend/frontend/src/CheckoutForm.js
@@ -1,13 +1,110 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
-import { CardElement } from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
+import { PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
 const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currency, setCurrency] = useState("");
+  const [amount, setAmount] = useState(1000);
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
+  useEffect(() => {
+    console.log(currency);
+  }, [currency]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: "http://localhost:3000/payment",
+        receipt_email: email,
+      },
+    });
+
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
+    } else {
+      setMessage("An unexpected error occured.");
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (currency === "ugx") {
+      setAmount(amount * 2844.75);
+    } else {
+      setAmount(1000);
+    }
+  }, [currency]);
+
   return (
-    <form>
-      <PaymentElement />
-      <button>Submit</button>
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <h2>
+        {" "}
+        Your total payable amount is ${amount} {currency}{" "}
+      </h2>
+      <select onChange={(e) => setCurrency(e.target.value)}>
+        <option value="ugx">UGX</option>
+        <option value="cad">CAD</option>
+      </select>
+      <input
+        id="email"
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter email address"
+      />
+      <PaymentElement id="payment-element" />
+      <button disabled={isLoading || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+        </span>
+      </button>
+      {/* Show any error or success messages */}
+      {message && <div id="payment-message">{message}</div>}
     </form>
   );
-  // return <CardElement />;
 };
 
 export default CheckoutForm;
--- frontend/frontend/src/StripePayment.js
+++ frontend/frontend/src/StripePayment.js
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
-// import StripeCheckout from "react-stripe-checkout";
-import { Elements } from "@stripe/react-stripe-js";
+import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
-const stripePromise = loadStripe("asd");
+import CheckoutForm from "./CheckoutForm";
+import "./App.css";
+
+const stripePromise = loadStripe("enter pk here");
 
 const StripePayment = () => {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8081/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
   return (
-    <Elements stripe={stripePromise}>
-      <CheckoutForm />
-    </Elements>
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
   );
 };
 
